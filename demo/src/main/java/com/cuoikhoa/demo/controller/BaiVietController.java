252320cf7fe6df50b9cc18d0550999cf6bdd068b
Quangzr <quangredmi3@gmail.com>
2022-02-14 13:14:04 +0700

    test commit 1

--- demo/src/main/java/com/cuoikhoa/demo/controller/BaiVietController.java
+++ demo/src/main/java/com/cuoikhoa/demo/controller/BaiVietController.java
@@ -24,6 +24,7 @@ import com.cuoikhoa.demo.service.QuyenHanService;
 import com.cuoikhoa.demo.service.TaiKhoanService;
 import com.cuoikhoa.demo.service.TinhTrangHocService;
 
+//alo
 @RestController
 public class BaiVietController {
 	//Autowired Repository
