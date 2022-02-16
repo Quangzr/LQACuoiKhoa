f7f6812c6592d1cde6444d626deedbdb19526025
Quangzr <quangredmi3@gmail.com>
2022-02-16 15:24:48 +0700

    login register demo

--- FE/cuoi-khoa-fe/src/App.js
+++ FE/cuoi-khoa-fe/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
+
+import SignIn from './LoginRegister/Login';
+import SignUp from './LoginRegister/Register';
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <Routes>
+            <Route path="/" element={ <SignIn /> }></Route> 
+            <Route path="/Register" element={ <SignUp /> }></Route> 
+        </Routes>
+
     </div>
   );
 }
